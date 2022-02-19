8a2eaf4b58a2db2812554176c3d2f9db720407e7
tomthomas1 <tomthomas8688@gmail.com>
2022-02-19 09:02:54 +0530

    Player plays a game for bet price

--- src/com/bridgelabz/Gambling.java
+++ src/com/bridgelabz/Gambling.java
@@ -13,6 +13,21 @@ public class Gambling {
 
 		System.out.println("The total stake per days is $" + StakePerDay);
 		System.out.println("Bet price per game is $" + BetPrice);
+
+		int play = (int) (Math.random() * 2);
+		switch (play) {
+		case 0:
+			StakePerDay += BetPrice;
+			System.out.println(" \n The player has won $1");
+			System.out.println("The stake after winning the game is $" + StakePerDay);
+			break;
+
+		case 1:
+			StakePerDay -= BetPrice;
+			System.out.println(" \n The player has lost $1");
+			System.out.println("The stake after lossing the game is $" + StakePerDay);
+			break;
+		}
 	}
 
 }
