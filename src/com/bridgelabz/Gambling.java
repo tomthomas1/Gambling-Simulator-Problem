ea13a1c11c619cdc08a9f2084a17b41e9b61cc2d
tomthomas1 <tomthomas8688@gmail.com>
2022-02-21 02:12:52 +0530

    Luckiest day and unlukiest day

--- src/com/bridgelabz/Gambling.java
+++ src/com/bridgelabz/Gambling.java
@@ -3,15 +3,15 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class Gambling {
-	int stakePerDay;
-	int betPrice = 1;
-	int minStake = 50;
-	int maxStake = 150;
-	int win = 0;
-	int loose = 0;
-	int profit = 0;
+	public static int stakePerDay, stake = 0, totalStake = 0;
+	public static int betPrice = 1;
+	public static int minStake = 50;
+	public static int maxStake = 150;
+	public static int win = 0, monthlyWins = 0, maxWin = 0;
+	public static int loose = 0, monthlyLoose = 0, maxLoss = 0;
+	public static int profit = 0, maxWinDay, maxLossDay;
 
-	public void play() {
+	public static void play() {
 		while (stakePerDay > minStake && stakePerDay < maxStake) // Win or Lost 50% of stake
 		{
 			int play = (int) (Math.random() * 2); // 0 = Win, 1 = Loose
@@ -29,16 +29,52 @@ public class Gambling {
 		}
 	}
 
-	public void gamble20Days() {
+	// for calculating the net profit or loss that the gambler has
+	public static void calculateStake() {
+		stake = stakePerDay - 100;
+		totalStake += stake;
+		System.out.println(" \n The net profit or loss is : " + totalStake);
+
+	}
+
+	// to calculate the maximum win for a day in a month
+	public static boolean calculateMaxWin() {
+		if (totalStake > maxWin) {
+			maxWin = totalStake;
+			return true;
+		}
+		return false;
+	}
+
+	// to calculate the maximum win for a day in a month
+	public static boolean calculateMaxLoss() {
+		if (totalStake < maxLoss) {
+			maxLoss = totalStake;
+			return true;
+
+		}
+		return false;
+
+	}
+
+	public static void gamble20Days() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the amount of Stake:");
 		int num = scanner.nextInt();
-		for (int days = 1; days <= 30; days++) { // loop for 20 days
+		for (int days = 1; days <= 30; days++) { // loop for 30 days
 			stakePerDay = num; // Accepting input for Stake Per day
 			play();
-			System.out.println(" \n The player has stake of " + stakePerDay + " on day " + days);
-			int balanceDay = stakePerDay - 100; // as the stake per day is $100 so the profit or loss is stakePrice -
-												// 100
+			calculateStake();
+			System.out.println("The player has stake of " + stakePerDay + " on day " + days);
+
+			if (calculateMaxWin()) {
+				maxWinDay = days;
+			}
+			if (calculateMaxLoss()) {
+				maxLossDay = days;
+			}
+
+			int balanceDay = stakePerDay - num; // as the stake per day is $100 so the profit or loss is stakePrice - num
 			profit += balanceDay;
 			if (stakePerDay > minStake) {
 				win++; // if player wins then the win days increments
@@ -47,13 +83,16 @@ public class Gambling {
 			}
 			System.out.println("The total profit or loss of the day is " + balanceDay);
 		}
-		System.out.println(" \n In total 30 days, the player has won " + win + " days and loose " + loose + " days");
-		System.out.println("The total gain amount for 30 Days is $" + profit); // printing the total profit
-		scanner.close();
 	}
 
 	public static void main(String[] args) {
 		Gambling gamble = new Gambling();
 		gamble.gamble20Days();
+		System.out.println(" \n In total 30 days, the player has won " + win + " days and loose " + loose + " days");
+		System.out.println("The total gain amount for 30 Days is $" + profit); // printing the total profit
+		System.out.println("Maximum Amount Won: " + maxWin);
+		System.out.println("Maximum Amount Won On Day " + maxWinDay);
+		System.out.println("Maximum Amount Loss: " + maxLoss);
+		System.out.println("Maximum Amount Loss On Day " + maxLossDay);
 	}
 }
